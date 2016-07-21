d7c4eb44492ab3daaf924265dd49477b2f5cbbf2
lkitching <lee@swirrl.com>
2016-07-21 14:11:14 +0100

    Implement basic eval and add built-in operations.
    Add PScheme.Eval module for evaluating forms within an environment.
    Numbers and strings evaluate to themselves, symbols resolve to their
    value in the environment, functions apply to evaluated arguments and
    special forms apply to unevaluated arguments.
    
    Add a default environment containing multiply, add and subtract
    functions along with an if special form.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,7 @@
 module Main where
 
 import PScheme.Reader
+import PScheme.Eval
 import Control.Monad (forever)
 import System.IO (hFlush, stdout)
 
@@ -12,15 +13,18 @@ formatError Incomplete = "Incomplete expression"
 formatError (ExpectedChar c) = "Expected '" ++ (show c) ++ "\""
 formatError (BadNumber s) = "Invalid number: " ++ s
 
-readEval :: String -> String
-readEval s = either formatError evalExpr (snd $ runRead s readExpr)
+showEval :: Env -> Expr -> String
+showEval env expr = either show show (eval env expr)
+
+readEval :: Env -> String -> String
+readEval env s = either formatError (showEval env) (snd $ runRead s readExpr)
 
 repl :: IO ()
 repl = do
   putStr "pscheme> "
   hFlush stdout
   l <- getLine
-  putStrLn $ readEval l
+  putStrLn $ readEval defaultEnv l
 
 main :: IO ()
 main = forever repl
---
+++ src/PScheme/Eval.hs
@@ -0,0 +1,82 @@
+module PScheme.Eval where
+
+import PScheme.Reader (Expr(..))
+import Data.Traversable (sequence)
+import qualified Data.Map.Strict as M
+
+data EvalError =
+    UnboundSymbol String
+  | OperatorRequired
+  | TypeError String PValue
+
+instance Show EvalError where
+  show (UnboundSymbol sym) = "Unbound symbol: " ++ sym
+  show OperatorRequired = "Operator required"
+  show (TypeError ty value) = "Unexpected type for " ++ (show value) ++ ": required " ++ ty
+
+data PValue =
+    PNumber Integer
+  | PStr String
+  | Fn ([PValue] -> EvalResult)
+  | Special (Env -> [Expr] -> EvalResult)
+
+instance Show PValue where
+  show (PNumber i) = show i
+  show (PStr s) = s
+  show (Fn _) = "<function>"
+  show (Special _) = "<special>"
+
+type EvalResult = Either EvalError PValue
+type Env = M.Map String PValue
+
+newEnv :: Env
+newEnv = M.empty
+
+applyFn :: Env -> ([PValue] -> EvalResult) -> [Expr] -> EvalResult
+applyFn env f es = (traverse (eval env) es) >>= (f $)
+
+applyOp :: Env -> PValue -> [Expr] -> EvalResult
+applyOp env (Fn f) es = applyFn env f es
+applyOp env (Special f) es = f env es
+applyOp _ _ _ = Left OperatorRequired
+
+expectNum :: PValue -> Either EvalError Integer
+expectNum (PNumber i) = Right i
+expectNum v = Left $ TypeError "Number" v
+
+arithFn :: ([Integer] -> Integer) -> [PValue] -> EvalResult
+arithFn f vs = do
+  nums <- traverse expectNum vs
+  return $ PNumber $ f nums
+
+plusFn :: [PValue] -> EvalResult
+plusFn = arithFn sum
+
+minusFn :: [PValue] -> EvalResult
+minusFn = arithFn sub where
+  sub [] = 0
+  sub [i] = negate i
+  sub ls = foldl1 (-) ls
+
+ifSpecial :: Env -> [Expr] -> EvalResult
+ifSpecial env [test, ifTrue, ifFalse] = do
+  b <- eval env test
+  n <- expectNum b
+  eval env (if n == 0 then ifFalse else ifTrue) 
+
+defaultEnv :: Env
+defaultEnv = M.fromList [("plus", (Fn plusFn)),
+                         ("minus", (Fn minusFn)),
+                         ("*", (Fn $ arithFn product)),
+                         ("if", (Special ifSpecial))]
+
+eval :: Env -> Expr -> EvalResult
+eval env expr = case expr of
+  Number i -> Right $ PNumber i
+  Str s -> Right $ PStr s
+  Symbol s -> case (M.lookup s env) of
+    Just v -> Right v
+    Nothing -> Left (UnboundSymbol s)
+  List l -> case l of
+    [] -> Left OperatorRequired
+    e:es -> (eval env e) >>= (\fe -> applyOp env fe es)  
