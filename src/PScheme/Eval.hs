320ef06a2518bd9c80e302d94c2be9290b3f5ca5
lkitching <lee@swirrl.com>
2016-07-24 16:56:46 +0100

    Add let special form.
    Change the type of the environment to be represented as a list
    of environment frames an allow new frames to be added and removed.
    
    Implement the let special form which evaulates a list of bindings
    in the current environment, adds them to a new environment frame
    and then evaluates the given body in the modified environment.

--- src/PScheme/Eval.hs
+++ src/PScheme/Eval.hs
@@ -8,11 +8,13 @@ data EvalError =
     UnboundSymbol String
   | OperatorRequired
   | TypeError String PValue
+  | FormError String [Expr]
 
 instance Show EvalError where
   show (UnboundSymbol sym) = "Unbound symbol: " ++ sym
   show OperatorRequired = "Operator required"
   show (TypeError ty value) = "Unexpected type for " ++ (show value) ++ ": required " ++ ty
+  show (FormError msg form) = "Invalid form: " ++ msg
 
 data PValue =
     PNumber Integer
@@ -32,10 +34,24 @@ isTruthy (PStr "") = False
 isTruthy _ = True
 
 type EvalResult = Either EvalError PValue
-type Env = M.Map String PValue
+type EnvFrame = M.Map String PValue
+type Env = [EnvFrame]
 
 newEnv :: Env
-newEnv = M.empty
+newEnv = []
+
+envLookup :: String -> Env -> Maybe PValue
+envLookup _ [] = Nothing
+envLookup k (e:es) = case (M.lookup k e) of
+  m@(Just _) -> m
+  Nothing -> envLookup k es
+
+pushEnv :: EnvFrame -> Env -> Env
+pushEnv = (:)
+
+popEnv :: Env -> Env
+popEnv [] = []
+popEnv (_:es) = es
 
 applyFn :: Env -> ([PValue] -> EvalResult) -> [Expr] -> EvalResult
 applyFn env f es = (traverse (eval env) es) >>= (f $)
@@ -66,19 +82,57 @@ minusFn = arithFn sub where
 ifSpecial :: Env -> [Expr] -> EvalResult
 ifSpecial env [test, ifTrue, ifFalse] = do
   b <- eval env test
-  eval env (if (isTruthy b) then ifTrue else ifFalse) 
+  eval env (if (isTruthy b) then ifTrue else ifFalse)
+ifSpecial _ exprs = Left $ FormError "if form requires test ifTrue and ifFalse expressions" exprs
+
+expectListExpr :: Expr -> Either EvalError [Expr]
+expectListExpr (List es) = Right es
+expectListExpr e = Left $ FormError "expected list" [e]
+
+expectPairExpr :: Expr -> Either EvalError (Expr, Expr)
+expectPairExpr e = do
+  l <- expectListExpr e
+  case l of
+    [e1, e2] -> Right (e1, e2)
+    _ -> Left $ FormError "expected pair" l
+
+expectSymbolExpr :: Expr -> Either EvalError String
+expectSymbolExpr (Symbol s) = Right s
+expectSymbolExpr e = Left $ FormError "expected symbol" [e]
+
+expectBindingPair :: Expr -> Either EvalError (String, Expr)
+expectBindingPair e = do
+  (nameExpr, valExpr) <- expectPairExpr e
+  varName <- expectSymbolExpr nameExpr
+  return (varName, valExpr)
+  
+letSpecial :: Env -> [Expr] -> EvalResult
+letSpecial env [bindingExpr, body] = do
+  bindingList <- expectListExpr bindingExpr
+  bindingValues <- traverse evalBindingPair bindingList
+  let ef = M.fromList bindingValues
+  eval (pushEnv ef env) body
+  where
+    evalBindingPair :: Expr -> Either EvalError (String, PValue)
+    evalBindingPair e = do
+      (name, expr) <- expectBindingPair e
+      v <- eval env expr
+      return (name, v)
+      
+letSpecial _ exprs = Left $ FormError "let form requires binding list and expression to evaluate" exprs
 
 defaultEnv :: Env
-defaultEnv = M.fromList [("+", (Fn plusFn)),
-                         ("-", (Fn minusFn)),
-                         ("*", (Fn $ arithFn product)),
-                         ("if", (Special ifSpecial))]
+defaultEnv = [M.fromList [("+", (Fn plusFn)),
+                          ("-", (Fn minusFn)),
+                          ("*", (Fn $ arithFn product)),
+                          ("if", (Special ifSpecial)),
+                          ("let", (Special letSpecial))]]
 
 eval :: Env -> Expr -> EvalResult
 eval env expr = case expr of
   Number i -> Right $ PNumber i
   Str s -> Right $ PStr s
-  Symbol s -> case (M.lookup s env) of
+  Symbol s -> case (envLookup s env) of
     Just v -> Right v
     Nothing -> Left (UnboundSymbol s)
   List l -> case l of
