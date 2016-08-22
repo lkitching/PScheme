eeda6f00139043afbd6d333fea3a14ba054718ed
lkitching <lee@swirrl.com>
2016-08-23 00:07:26 +0100

    Evaluate file if argument given on command line.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,62 @@
 module Main where
 
 import PScheme.Repl
+import PScheme.Reader
+import PScheme.Eval (exceptT, defaultEnv, runEval, eval)
+import System.IO
+import System.Environment (getArgs)
+import System.Exit (exitFailure)
+import Control.Monad (forM_)
+import Control.Monad.Trans.Class (lift)
+import Control.Monad.Trans.Except
+import Control.Monad.IO.Class (liftIO, MonadIO)
+
+parseForms :: [[Token]] -> Either ReadError [Value]
+parseForms [] = pure []
+parseForms (ts:tss) = do
+  state <- parse ts
+  parseLines tss state
+
+parseLines :: [[Token]] -> ParseOutcome -> Either ReadError [Value]
+parseLines [] state = case (parsedValue state) of
+  Nothing -> pure []
+  Just v -> pure [v]
+parseLines (ts:tss) state = case (parsedValue state) of
+  Nothing -> (parseNext state ts) >>= (parseLines tss)
+  Just v -> do
+    state' <- parseNext state ts
+    vs <- parseLines tss state'
+    pure (v:vs)
+
+tokeniseAll :: String -> Either ReadError [[Token]]
+tokeniseAll s = tokeniseLines (lines s) where
+  tokeniseLines [] = pure []
+  tokeniseLines (l:ls) = do
+    ts <- readTokens l
+    tss <- tokeniseLines ls
+    pure $ ts:tss
+
+readHandle :: Handle -> ExceptT ReadError IO [Value]
+readHandle h = do
+  contents <- liftIO $ hGetContents h
+  lineTokens <- exceptT $ tokeniseAll contents
+  exceptT $ parseForms lineTokens
+  
+evalFile :: FilePath -> IO ()
+evalFile filePath = withFile filePath ReadMode $ \h -> do
+  result <- runExceptT $ readHandle h
+  case result of
+    Left err -> print err
+    Right forms -> do
+      env <- defaultEnv
+      forM_ forms (\f -> fmap (either show show) (runEval env (eval f)) >>= print)
 
 main :: IO ()
-main = repl
+main = do
+  args <- getArgs
+  case args of
+    [] -> repl
+    [filePath] -> evalFile filePath
+    _ -> do
+      hPutStrLn stderr "Usage: pscheme [file]"
+      exitFailure
