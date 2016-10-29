4c044d6d433a05d439f7b4cc341a9887b407b7cd
lkitching <lee.kitching@gmail.com>
2016-10-29 15:24:15 +0100

    Add property for reading a stream of tokens.
    Add quickcheck property for reading a stream of tokens from a string.

--- test/ReaderTests.hs
+++ test/ReaderTests.hs
@@ -5,6 +5,8 @@ import Test.Tasty.QuickCheck as QC
 import Test.Tasty.HUnit
 import Test.QuickCheck
 
+import Data.List (foldl')
+
 import PScheme.Reader
 
 genIdent :: Gen String
@@ -55,15 +57,30 @@ instance Arbitrary Value where
   shrink (Cons h t) = [h, t]
   shrink _ = []
 
+--checks a single expression can be re-read
 read_prop :: Value -> Bool
 read_prop v = case readStringOne (show v) of
   Left _ -> False
   Right r -> v == r
 
+--checks a single token can be re-read
 tokenise_prop :: Token -> Bool
 tokenise_prop t = case readTokens (show t) of
   Right [t'] -> t == t'
   _ -> False
+
+needsDelim :: Token -> Bool
+needsDelim (TNumber _ _) = True
+needsDelim (TSym _) = True
+needsDelim _ = False
+  
+formatTokenStream :: [Token] -> String
+formatTokenStream = concat . map (\t -> if needsDelim t then ' ':(show t) else (show t))
+
+--checks a list of tokens can be re-tokenised correctly
+token_stream_prop :: [Token] -> Bool
+token_stream_prop ts =
+  either (const False) (== ts) $ readTokens (formatTokenStream ts)
 
 readerTests :: TestTree
 readerTests = testGroup "Reader Tests" [qcTests, unitTests]
@@ -71,7 +88,8 @@ readerTests = testGroup "Reader Tests" [qcTests, unitTests]
 qcTests :: TestTree
 qcTests = testGroup "Reader quickcheck tests"
   [QC.testProperty "read" read_prop,
-   QC.testProperty "tokenise" tokenise_prop]
+   QC.testProperty "tokenise" tokenise_prop,
+   QC.testProperty "token stream" token_stream_prop]
 
 unitTests :: TestTree
 unitTests = testGroup "Reader unit tests"
